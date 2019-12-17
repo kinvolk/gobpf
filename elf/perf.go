a5f1167657b6277a6e5628d6dd575c155566e4c2
Kai Lüke <kai@kinvolk.io>
2019-12-17 12:47:03 +0100

    wip: end markers

--- elf/perf.go
+++ elf/perf.go
@@ -129,7 +129,7 @@ static int perf_event_dump_backward(int page_count, int page_size, void *_state,
 	}
 
 	if ((state->wrapped && state->data_head >= data_head) || state->wrapped > 1) {
-		return 0;
+		return -1;
 	}
 
 	begin = p = base + state->data_head;
@@ -203,7 +203,12 @@ func InitPerfMap(b *Module, mapName string, receiverChan chan []byte, lostChan c
 	}, nil
 }
 
-func (pm *PerfMap) SwapAndDumpBackward() (out [][]byte) {
+// When the buffer overflowed and the oldest events were overwritten, the caller can construct
+// an "events lost" indicator event based on the last retained event (e.g., to use a similar timestamp).
+// The first argument is the CPU ID. On error no "events lost" indicator is appended to the list of events.
+type MakeLostEventIndicatorFromLastEvent func(uint16, [] byte) ([]byte, error)
+
+func (pm *PerfMap) SwapAndDumpBackward(makeLostEvent MakeLostEventIndicatorFromLastEvent) (out [][]byte) {
 	m, ok := pm.program.maps[pm.name]
 	if !ok {
 		// should not happen or only when pm.program is
@@ -233,7 +238,7 @@ func (pm *PerfMap) SwapAndDumpBackward() (out [][]byte) {
 	}
 
 	// step 3: dump old buffer
-	out = pm.DumpBackward()
+	out = pm.DumpBackward(makeLostEvent)
 
 	// step4: close old buffer
 	for _, fd := range m.pmuFDs {
@@ -249,7 +254,7 @@ func (pm *PerfMap) SwapAndDumpBackward() (out [][]byte) {
 	return
 }
 
-func (pm *PerfMap) DumpBackward() (out [][]byte) {
+func (pm *PerfMap) DumpBackward(makeLostEvent MakeLostEventIndicatorFromLastEvent) (out [][]byte) {
 	incoming := OrderedBytesArray{timestamp: pm.timestamp}
 
 	m, ok := pm.program.maps[pm.name]
@@ -270,7 +275,16 @@ func (pm *PerfMap) DumpBackward() (out [][]byte) {
 				unsafe.Pointer(&state), unsafe.Pointer(m.headers[cpu]),
 				unsafe.Pointer(&sample))
 			switch ok {
+			case -1:
+				// Finished but the buffer was full, indicating that something might have been lost
+				fmt.Printf("loss on cpu %d\n", cpu)
+				indicatorEvent, err := makeLostEvent(uint16(cpu), incoming.bytesArray[len(incoming.bytesArray)-1])
+				if err == nil {
+					incoming.bytesArray = append(incoming.bytesArray, indicatorEvent)
+				}
+				break ringBufferLoop
 			case 0:
+				fmt.Printf("%d %d\n", uint64(m.headers[cpu].data_head), uint64(uintptr(unsafe.Pointer(m.headers[cpu]))) + uint64(pageSize) + uint64(state.data_head))
 				break ringBufferLoop // nothing to read
 			case C.PERF_RECORD_SAMPLE:
 				size := sample.Size - 4
